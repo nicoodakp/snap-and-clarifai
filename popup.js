f6995e369237c902b3a59eda443780ffd40d3f14
kai.peng <kai.peng@uconn.edu>
2018-09-26 17:26:14 -0400

    close spaces

--- popup.js
+++ popup.js
@@ -11,7 +11,10 @@ try {
 	});
 }
 catch(err) {
-	alert("Need a valid API Key!");
+	var nokey = document.createElement("p");
+	nokey.setAttribute('class', 'error');
+	nokey.innerText = "Need a valid API key.";
+	document.body.appendChild(nokey);
 	throw "Invalid API Key";
 }
 
@@ -26,14 +29,9 @@ var imgb64src, width, height, imgdetail, canvas, ctx;
          height = $('img').height();
 		 	imgdetail = imgb64src.replace(/^data:image\/(.*);base64,/, '');  // pure b64 string for Clarifai
 			console.log(height);
-
-
-
 				}
 	catch(err) {
-				alert("I can't seem to finding any videos...");
-				// throw "I can't seem to finding any videos...";
-				// $('.load-wrapp').hide();
+
 				return false;
 							}
 			//// clarifai there
@@ -45,24 +43,19 @@ var imgb64src, width, height, imgdetail, canvas, ctx;
 // });  // end chrome.tabs.onUpdated.addListener
 
 /// once the click generate successful, bind even to the link, and open tab benhind
-
 $('body').on('click', 'a.linky', function (){
 			chrome.tabs.create({active: false, url: $(this).attr('href')});
 				return false;
-		// console.log("click work?");
-				});
+						});
 
 
-/// draw bounding box while hover to name
-
+/// hightlight individual bounding box while hover to name
+for(var i = 0; i < $( 'canvas' ).length; i++) {
 $( 'body' ).on('mouseenter', 'a.linky',  function () {
 			console.log($( 'canvas' ).length);
-		for(var i = 0; i < $( 'canvas' ).length; i++) {
-		$( 'canvas' ).show(1);
-	}
-
-
-					});
+		document.getElementsByClassName('canvas')[i].style.visibility = 'show';
+						});
+					}
 /// end draw box
 
 
@@ -74,23 +67,14 @@ function doPredict(value) {
 				console.log(response); // test response, get familiar with the API strucure
 				var name, prob, boundingbox;
 
-
-
-
 		// check for region and concepts tag in API, if none then nothing was found.
 		// if found then its a successful call
 				if(response.outputs[0].data.hasOwnProperty("regions") ||
 						response.outputs[0].data.hasOwnProperty("concepts")){
 				regionArray = response.outputs[0].data.regions;
 
-				// canvas set up
-	// $('img').after( '<canvas id = "canvas"></canvas>' ); /// canvas ready for draw face box
-			// end, also adjust the position through css, so now its overlay on img
-
-
 // if found, then for i , grab these datas
-				for(var i = 0; i < regionArray.length; i++) {
-
+			for(var i = 0; i < regionArray.length; i++) {
 
 				         name = response.outputs[0].data.regions[i].data.face.identity.concepts[0].name;
 					   fullname = capitalize(name);
@@ -111,7 +95,7 @@ function doPredict(value) {
 			// drawandconnect(x,y,w,h);
 
 			// canvas set up
-			$('img').after( '<canvas id = "canvas"></canvas>' ); /// canvas ready for draw face box
+			$('img').after( '<canvas id =  canvas></canvas>' ); /// canvas ready for draw face box
 		// end, also adjust the position through css, so now its overlay on img
 			var c = document.getElementById("canvas");
 			var ctx = c.getContext("2d");
@@ -119,24 +103,18 @@ function doPredict(value) {
 			c.height = h*(.985);
 			c.style.left = x*1.84855+"px";
 			c.style.top = y*(0.59582)+"px";
-			console.log("x are"+" " + x);
 
-			// ctx.strokeStyle = "#FF0000";
-			// ctx.strokeRect(x, y, w, h);
-			console.log(x);
 				 // end gather box data
 					console.log(response.outputs[0].data.regions[i].data.face.identity.concepts[0].name);
 
 					var source = "https://www.youtube.com/results?search_query="+fullname.split(' ').join('+');
 					var catDiv = document.createElement("p");
-					console.log(typeof source);									//"target=" + "_blank" +
 					catDiv.innerHTML = "Name:" + " " + "<a"  + " " + " " + "class=" + "linky" + " " + " " + "href=" +  source + ">" + fullname + "</a>" + " " +   "" + percentageprob;
 					// catDiv.setAttribute('alt', 'namee');
 					catDiv.setAttribute('class', 'namee');
 					catDiv.setAttribute('align', 'center');
 					document.body.appendChild(catDiv);
 					$('.load-wrapp').hide();
-
 
 				} // end check for region for loop
 			} // end if
@@ -153,10 +131,6 @@ function doPredict(value) {
 }); /// end doc ready function
 
 // draw box - user hover name, show bounding boundingbox
-
-
-
-
 //  Purpose: Return a capitalized String
 //  Args:
 //    s - A String
